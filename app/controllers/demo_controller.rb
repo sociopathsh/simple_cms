5a73cd3aeb801a4cd514a625424190b3fef8b0fb
SoeHtut <soehtut@escapepixel.com>
2018-08-15 11:27:17 +0630

    render a template

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,4 +1,19 @@
 class DemoController < ApplicationController
+
+  # def index
+  # end
+
+  # def hello
+  # end
+
   def index
+    # render('demo/hello')
+    render('hello')
   end
+
+  def hello
+    # render('demo/index')
+    render('index')
+  end
+  
 end
